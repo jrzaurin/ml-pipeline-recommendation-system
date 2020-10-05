2ade7eaf511b1723299b6bad139a7379134442fc
nadbor <nadbordrozd@gmail.com>
2020-10-05 11:50:32 +0000

    uploading raw data to s3

---
+++ x02_upload.sh
@@ -0,0 +1,7 @@
+s3cmd put data/raw/*json s3://recsys-1/raw/
+s3cmd put data/raw/*csv s3://recsys-1/raw/
+s3cmd put data/raw/*gz s3://recsys-1/raw/
+s3cmd put metadata_split/* s3://recsys-1/raw/metadata_split
+s3cmd put metadata_split/* s3://recsys-1/raw/metadata_split/
+s3cmd put reviews_split/* s3://recsys-1/raw/reviews_split
+s3cmd put reviews_split/* s3://recsys-1/raw/reviews_split/
