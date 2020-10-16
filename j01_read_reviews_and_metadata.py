1b61beb577178e4e21b20fc6e82f24ea3e5d1479
jrzaurin <jrzaurin@gmail.com>
2020-10-17 01:03:59 +0200

    started with the eda for the metadata. Not much there but something can be done

---
+++ j01_read_reviews_and_metadata.py
@@ -0,0 +1,38 @@
+import gc
+# import json
+from pathlib import Path
+
+# import dask.bag as db
+import pandas as pd
+
+pd.options.display.max_columns = 100
+RAW_DATA_DIR = Path("data/raw/amazon")
+
+# def flatten(record):
+#     return {k: v for k, v in record.items()}
+
+if __name__ == "__main__":
+
+    df = pd.read_json(RAW_DATA_DIR / "Movies_and_TV.json.gz", lines=True)
+    # feather, parquet...we will decide in due time
+    df.to_feather(RAW_DATA_DIR / "Movies_and_TV.f")
+    del df
+    gc.collect()
+
+    df = pd.read_json(RAW_DATA_DIR / "meta_Movies_and_TV.json.gz", lines=True)
+    df.to_pickle(RAW_DATA_DIR / "meta_Movies_and_TV.p")
+    del df
+    gc.collect()
+
+    # # or alternatively
+    # reviews = db.read_text(RAW_DATA_DIR / "Movies_and_TV.json.gz").map(json.loads)
+    # df = reviews.map(flatten).to_dataframe()
+    # df.to_parquet("raw_data/Movies_and_TV.parquet")
+    # del (reviews, df)
+    # gc.collect()
+
+    # reviews = db.read_text(RAW_DATA_DIR / "meta_Movies_and_TV.json.gz").map(json.loads)
+    # df = reviews.map(flatten).to_dataframe()
+    # df.to_parquet("raw_data/meta_Movies_and_TV.parquet")
+    # del (reviews, df)
+    # gc.collect()
---
+++ upload_to_s3.sh
@@ -0,0 +1,3 @@
+aws s3 cp data/raw/amazon/Movies_and_TV.json.gz s3://recsys-2/data/raw/amazon/Movies_and_TV.json.gz
+aws s3 cp data/raw/amazon/meta_Movies_and_TV.json.gz s3://recsys-2/data/raw/amazon/meta_Movies_and_TV.json.gz
+
