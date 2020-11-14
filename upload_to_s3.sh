3f6780ae16bf89890c43b4cba60c11ecabadbd02
jrzaurin <jrzaurin@gmail.com>
2020-11-15 00:43:24 +0100

    first step towards adding NeuCF. Mostly copying and pasting from my RecoTour repo

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -4,3 +4,8 @@ aws s3 cp data/raw/amazon/meta_Movies_and_TV.json.gz s3://recsys-2/data/raw/amaz
 aws s3 cp s3://recsys-1/raw/Clothing_Shoes_and_Jewelry.json.gz data/raw/amazon/Clothing_Shoes_and_Jewelry.json.gz
 aws s3 cp s3://recsys-1/raw/meta_Clothing_Shoes_and_Jewelry.json.gz data/raw/amazon/meta_Clothing_Shoes_and_Jewelry.json.gz
 
+aws s3 cp data/processed/amazon/ s3://recsys-2/data/processed/amazon/ --recursive
+
+aws s3 cp s3://recsys-2/data/processed/amazon/ data/processed/amazon/  --recursive
+aws s3 cp s3://recsys-2/data/raw/amazon/ data/raw/amazon/  --recursive
+
