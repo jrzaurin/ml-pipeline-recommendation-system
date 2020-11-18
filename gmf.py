72002015ef9b4f09763cd6264494733bcea9b5d1
jrzaurin <jrzaurin@gmail.com>
2020-11-18 01:04:26 +0100

    tested the gmf implementation. Seems to perform amazingly well. More test tomorrow and then we will run experiments

--- gmf.py
+++ gmf.py
@@ -1,5 +1,11 @@
-import argparse
+import heapq
+import math
 import os
+import pickle
+from datetime import datetime
+# from functools import partial
+# from multiprocessing import Pool, cpu_count
+from pathlib import Path
 from time import time
 
 import numpy as np
@@ -7,73 +13,15 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 from torch import nn
-# from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from tqdm import tqdm, trange
 
-from metrics import hit_ratio, ndcg_binary
+from gmf_parser import parse_args
 
+# from metrics import hit_ratio, ndcg_binary
 
-def parse_args():
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--datadir", type=str, default="data/processed/amazon", help="data directory."
-    )
-    parser.add_argument(
-        "--modeldir",
-        type=str,
-        default="models",
-        help="models directory",
-    )
-    parser.add_argument(
-        "--resultsdir",
-        type=str,
-        default="results",
-        help="results directory",
-    )
-    parser.add_argument(
-        "--dataname",
-        type=str,
-        default="leave_one_out_w_negative_full_valid.npz",
-        help="npz file with dataset",
-    )
-    parser.add_argument("--epochs", type=int, default=20, help="number of epochs.")
-    parser.add_argument("--batch_size", type=int, default=256, help="batch size.")
-    parser.add_argument("--n_emb", type=int, default=8, help="embedding size.")
-    parser.add_argument(
-        "--lr", type=float, default=0.01, help="if lr_scheduler this will be max_lr"
-    )
-    parser.add_argument(
-        "--learner",
-        type=str,
-        default="adamw",
-        help="Specify an optimizer: adamw or sgd",
-    )
-    # parser.add_argument(
-    #     "--lr_scheduler",
-    #     action="store_true",
-    #     help="boolean to set the use of ReduceLROnPlateau during training",
-    # )
-    parser.add_argument(
-        "--validate_every", type=int, default=1, help="validate every n epochs"
-    )
-    parser.add_argument("--save_model", type=int, default=1)
-    parser.add_argument(
-        "--n_neg",
-        type=int,
-        default=4,
-        help="number of negative instances to consider per positive instance",
-    )
-    parser.add_argument(
-        "--topk",
-        type=int,
-        default=10,
-        help="number of items to retrieve for recommendation",
-    )
-
-    return parser.parse_args()
+use_cuda = torch.cuda.is_available()
 
 
 class GMF(nn.Module):
@@ -104,20 +52,49 @@ class GMF(nn.Module):
         return preds
 
 
-def get_metrics(group, k):
-    df = group[1]
-    true = df[df.score != 0]["item"].values
-    rec = df.sort_values("preds", ascending=False)["item"].values[:k]
-    return (hit_ratio(rec, true, k), ndcg_binary(rec, true, k))
+def hit_ratio_ncf(ranklist, gtitem):
+    if gtitem in ranklist:
+        return 1
+    return 0
 
 
-def sample_train_neg_instances(
+def ndcg_binary_ncf(ranklist, gtitem):
+    for i in range(len(ranklist)):
+        item = ranklist[i]
+        if item == gtitem:
+            return math.log(2) / math.log(i + 2)
+    return 0
+
+
+def get_metrics_ncf(items, preds, topk):
+
+    gtitem = items[0]
+
+    # the following 3 lines of code ensure that the fact that the 1st item is
+    # gtitem does not affect the final rank
+    randidx = np.arange(100)
+    np.random.shuffle(randidx)
+    items, preds = items[randidx], preds[randidx]
+
+    map_item_score = dict(zip(items, preds))
+    ranklist = heapq.nlargest(topk, map_item_score, key=map_item_score.get)
+    hr = hit_ratio_ncf(ranklist, gtitem)
+    ndcg = ndcg_binary_ncf(ranklist, gtitem)
+    return hr, ndcg
+
+
+# def get_metrics(group, k):
+#     df = group[1]
+#     true = df[df.rating != 0]["item"].values
+#     rec = df.sort_values("preds", ascending=False)["item"].values[:k]
+#     return (hit_ratio(rec, true, k), ndcg_binary(rec, true, k))
+
+
+def _sample_train_neg_instances(
     train_pos, train_wo_neg_lookup, test_w_neg_lookup, n_items, n_neg
 ):
-
     user, item, labels = [], [], []
     for u, i, r in tqdm(train_pos, desc="Sample Train Negatives"):
-
         # we need to make sure they are not in the negative examples used for
         # testing
         try:
@@ -142,7 +119,7 @@ def get_train_instances(train_wo_neg, test_w_neg, n_items, n_neg=4):
 
     train_wo_neg_lookup = train_wo_neg.groupby("user")["item"].apply(list)
     test_w_neg_lookup = test_w_neg.groupby("user")["item"].apply(list)
-    train_neg = sample_train_neg_instances(
+    train_neg = _sample_train_neg_instances(
         train_wo_neg.values,
         train_wo_neg_lookup,
         test_w_neg_lookup,
@@ -162,16 +139,11 @@ def get_train_instances(train_wo_neg, test_w_neg, n_items, n_neg=4):
     return train_w_neg.values
 
 
-def checkpoint(model, modelpath):
-    torch.save(model.state_dict(), modelpath)
-
-
 def train(
     model,
     optimizer,
     epoch,
     batch_size,
-    use_cuda,
     train_wo_neg,
     test_w_neg,
     n_items,
@@ -187,9 +159,7 @@ def train(
     with trange(train_steps) as t:
         for batch_idx, data in zip(t, train_loader):
             t.set_description("epoch: {}".format(epoch + 1))
-            users = data[:, 0]
-            items = data[:, 1]
-            labels = data[:, 2].float()
+            users, items, labels = data[:, 0], data[:, 1], data[:, 2].float()
             if use_cuda:
                 users, items, labels = users.cuda(), items.cuda(), labels.cuda()
             optimizer.zero_grad()
@@ -197,20 +167,31 @@ def train(
             loss = F.binary_cross_entropy(preds.squeeze(1), labels)
             loss.backward()
             optimizer.step()
-
             running_loss += loss.item()
             avg_loss = running_loss / (batch_idx + 1)
-
             t.set_postfix(loss=avg_loss)
-
     return avg_loss
 
 
-def evaluate(model, test_loader, use_cuda, topk):
+# def evaluate(model, eval_loader):
+#     model.eval()
+#     eval_preds = []
+#     with torch.no_grad():
+#         for data in tqdm(eval_loader, desc="Valid"):
+#             users, items, labels = data[:, 0], data[:, 1], data[:, 2].float()
+#             if use_cuda:
+#                 users, items, labels = users.cuda(), items.cuda(), labels.cuda()
+#             preds = model(users, items)
+#             preds_cpu = preds.squeeze(1).detach().cpu().numpy()
+#             eval_preds += [preds_cpu]
+#     return np.hstack(eval_preds)
+
+
+def evaluate_ncf(model, eval_loader):
     model.eval()
-    eval_preds = []
+    scores = []
     with torch.no_grad():
-        for data in test_loader:
+        for data in tqdm(eval_loader, desc="Valid"):
             users = data[:, 0]
             items = data[:, 1]
             labels = data[:, 2].float()
@@ -218,70 +199,72 @@ def evaluate(model, test_loader, use_cuda, topk):
                 users, items, labels = users.cuda(), items.cuda(), labels.cuda()
             preds = model(users, items)
             preds_cpu = preds.squeeze(1).detach().cpu().numpy()
-            eval_preds += [preds_cpu]
-    return np.vstack(eval_preds)
+
+            split_chuncks = preds_cpu.shape[0] // 100
+            items_cpu = items.cpu().numpy()
+            item_chunks = np.split(items_cpu, split_chuncks)
+            pred_chunks = np.split(preds_cpu, split_chuncks)
+            scores += [
+                get_metrics_ncf(it, pr, topk)
+                for it, pr in zip(item_chunks, pred_chunks)
+            ]
+
+    hr = [s[0] for s in scores]
+    ndcg = [s[1] for s in scores]
+    return (np.array(hr).mean(), np.array(ndcg).mean())
+
+
+def early_stopping(curr_value, best_value, stop_step, patience):
+    if curr_value >= best_value:
+        stop_step, best_value = 0, curr_value
+    else:
+        stop_step += 1
+    if stop_step >= patience:
+        print(
+            "Early stopping triggered. patience: {} log:{}".format(patience, best_value)
+        )
+        stop = True
+    else:
+        stop = False
+    return best_value, stop_step, stop
 
 
 if __name__ == "__main__":  # noqa: C901
 
-    datadir = "data/processed/amazon"
-    modeldir = "models"
-    resultsdir = "results"
-    dataname = "leave_one_out_w_negative_full_valid.npz"
-    n_emb = 8
-    lr = 0.01
-    batch_size = 1024
-    epochs = 1
-    learner = "adamw"
-    topk = 4
-    n_neg = 4
-    topk = 10
-    validate_every = 1
-    save_model = False
+    args = parse_args()
 
-    # args = parse_args()
+    DATA_DIR = Path(args.datadir)
+    MODEL_DIR = Path("models")
+    RESULTS_DIR = Path("results")
+    dataname = args.dataname
+    model_name = "_".join(["gmf", str(datetime.now()).replace(" ", "_")])
 
-    # # dirs and filenames
-    # datadir = args.datadir
-    # modeldir = args.modeldir
-    # resultsdir = args.resultsdir
-    # dataname = args.dataname
+    # model params
+    n_emb = args.n_emb
 
-    # # model params
-    # n_emb = args.n_emb
+    # train params
+    batch_size = args.batch_size
+    n_epochs = args.n_epochs
+    learner = args.learner
+    lr = args.lr
+    lr_scheduler = args.lr_scheduler
+    lr_patience = args.lr_patience
 
-    # # train params
-    # batch_size = args.batch_size
-    # epochs = args.epochs
-    # learner = args.learner
-    # # lr = args.lr
-    # # lr_scheduler = args.lr_scheduler
-    # # lrs = "wlrs" if lr_scheduler else "wolrs"
+    # eval params
+    topk = args.topk
+    n_neg = args.n_neg
+    eval_every = args.eval_every
+    early_stop_patience = args.early_stop_patience
 
-    # # eval params
-    # topk = args.topk
-    # n_neg = args.n_neg
-    # validate_every = args.validate_every
+    # save
+    save_results = args.save_results
 
-    # # save
-    # save_model = args.save_model
-
-    modelfname = (
-        "GMF"
-        + "_".join(["_bs", str(batch_size)])
-        # + "_".join(["_lr", str(lr).replace(".", "")])
-        + "_".join(["_n_emb", str(n_emb)])
-        + "_".join(["_lrnr", learner])
-        # + "_".join(["_lrs", lrs])
-        + ".pt"
-    )
-    if not os.path.exists(modeldir):
-        os.makedirs(modeldir)
-    modelpath = os.path.join(modeldir, modelfname)
-    resultsdfpath = os.path.join(modeldir, "results_df.p")
+    for d in [RESULTS_DIR, MODEL_DIR]:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
     # build or load train and test datasets
-    dataset = np.load(os.path.join(datadir, dataname))
+    dataset = np.load(DATA_DIR / dataname)
     train_wo_neg = pd.DataFrame(dataset["train"], columns=["user", "item", "rating"])
     test_w_neg = pd.DataFrame(dataset["test"], columns=["user", "item", "rating"])
     # we will treat it as a binary problem: interaction or not
@@ -290,12 +273,13 @@ if __name__ == "__main__":  # noqa: C901
     n_users, n_items = dataset["n_users"], dataset["n_items"]
 
     # test loader for validation
-    test_loader = DataLoader(dataset=test_w_neg.values, batch_size=1000, shuffle=False)
+    eval_loader = DataLoader(dataset=test_w_neg.values, batch_size=1000, shuffle=False)
 
     # model definition
     model = GMF(n_users, n_items, n_emb=n_emb)
+    if use_cuda:
+        model = model.cuda()
 
-    # set the optimizer and loss
     if learner.lower() == "adamw":
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     else:
@@ -303,70 +287,73 @@ if __name__ == "__main__":  # noqa: C901
             model.parameters(), lr=lr, momentum=0.9, nesterov=True
         )
 
-    use_cuda = torch.cuda.is_available()
-    if use_cuda:
-        model = model.cuda()
+    if lr_scheduler:
+        scheduler = ReduceLROnPlateau(
+            optimizer,
+            mode="max",
+            patience=lr_patience,
+            factor=0.5,
+            threshold=0.001,
+            threshold_mode="rel",
+        )
 
-    # --> HERE: early stop and save results and model
-    best_hr, best_ndcgm, best_iter = 0, 0, 0
-    for epoch in range(epochs):
+    best_score = -np.inf
+    stop_step = 0
+    update_count = 0
+    stop = False
+    for epoch in range(n_epochs):
         t1 = time()
         train(
             model,
             optimizer,
             epoch,
             batch_size,
-            use_cuda,
             train_wo_neg,
             test_w_neg,
             n_items,
             n_neg,
         )
         t2 = time()
-        if epoch % validate_every == 0:
-            res = evaluate(model, test_loader, use_cuda, topk)
-            hr = np.mean([el[0] for el in res])
-            ndcg = np.mean([el[1] for el in res])
+        if epoch % eval_every == (eval_every - 1):
+            # preds = evaluate(model, eval_loader)
+            # test_w_neg["preds"] = preds
+            # user_groups = test_w_neg.groupby("user")
+
+            # with Pool(cpu_count()) as p:
+            #     res = p.map(partial(get_metrics, k=10), [g for g in user_groups])
+
+            # hr = np.mean([el[0] for el in res])
+            # ndcg = np.mean([el[1] for el in res])
+
+            hr, ndcg = evaluate_ncf(model, eval_loader)
+
+            early_stop_score = ndcg
+            best_score, stop_step, stop = early_stopping(
+                early_stop_score,
+                best_score,
+                stop_step,
+                early_stop_patience,
+            )
+            if lr_scheduler:
+                scheduler.step(early_stop_score)
+            print("=" * 80)
             print(
-                "Epoch: {} {:.2f}s,  HR = {:.4f}, NDCG = {:.4f}, validated in {:.2f}s".format(
-                    epoch, t2 - t1, hr, ndcg, time() - t2
+                "HR = {:.4f}, NDCG = {:.4f}, validated in {:.2f}s".format(
+                    hr, ndcg, time() - t2
                 )
             )
-            if hr > best_hr:
-                best_hr, best_ndcg, best_iter, train_time = (
-                    hr,
-                    ndcg,
-                    epoch,
-                    t2 - t1,
-                )
-                if save_model:
-                    checkpoint(model, modelpath)
+            print("=" * 80)
+        if stop:
+            break
+        if (stop_step == 0) & (save_results):
+            best_epoch = epoch
+            torch.save(model.state_dict(), MODEL_DIR / (model_name + ".pt"))
 
-    print(
-        "End. Best Iteration {}: HR = {:.4f}, NDCG = {:.4f}. ".format(
-            best_iter, best_hr, best_ndcg
-        )
-    )
-    if save_model:
-        print("The best GMF model is saved to {}".format(modelpath))
-
-    # if save_model:
-    #     cols = [
-    #         "modelname",
-    #         "iter_loss",
-    #         "best_hr",
-    #         "best_ndcg",
-    #         "best_iter",
-    #         "train_time",
-    #     ]
-    #     vals = [modelfname, iter_loss, best_hr, best_ndcg, best_iter, train_time]
-    #     if not os.path.isfile(resultsdfpath):
-    #         results_df = pd.DataFrame(columns=cols)
-    #         experiment_df = pd.DataFrame(data=[vals], columns=cols)
-    #         results_df = results_df.append(experiment_df, ignore_index=True)
-    #         results_df.to_pickle(resultsdfpath)
-    #     else:
-    #         results_df = pd.read_pickle(resultsdfpath)
-    #         experiment_df = pd.DataFrame(data=[vals], columns=cols)
-    #         results_df = results_df.append(experiment_df, ignore_index=True)
-    #         results_df.to_pickle(resultsdfpath)
+    if save_results:
+        # Save results
+        results_d = {}
+        results_d["args"] = args.__dict__
+        results_d["best_epoch"] = best_epoch
+        results_d["ndcg"] = ndcg
+        results_d["hr"] = hr
+        pickle.dump(results_d, open(str(RESULTS_DIR / (model_name + ".p")), "wb"))
